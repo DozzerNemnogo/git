a08ea428bbd4b44689685bbaaa3a24322daef92a
Anton Afinogenov <dozzernemnogo@MacBook-Air-Anton.local>
2019-08-21 20:02:09 +0300

    Initial commit

---
+++ git/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  git
+//
+//  Created by Anton Afinogenov on 21/08/2019.
+//  Copyright © 2019 Anton Afinogenov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello World!")
+    }
+
+
+}
+
